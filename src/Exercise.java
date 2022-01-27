567c93f7d4b1dbc841ee1ed6e940fc9abf295d92
McStravagent <98091575+McStravagent@users.noreply.github.com>
2022-01-27 12:47:09 +0000

    Exercise1
    Working through FDM exercises

---
+++ src/Exercise.java
@@ -0,0 +1,135 @@
+public class Exercise {
+
+    public void main(String[] args){
+
+       /* System.out.println("this is a message");
+
+        String firstName = "John";
+        String lastName = "Smith";
+        String fullName = firstName+" "+lastName;
+
+        System.out.println(fullName);
+
+        String name = "Eric";
+
+        System.out.println("Hello "+name+" would you like to learn some Java today?");
+
+        String name = "Nick";
+
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(3));
+
+        String sentence = "this is a sentence";
+        System.out.println(sentence.indexOf(' '));
+        System.out.println(sentence.lastIndexOf(' '));
+        System.out.println(sentence.substring(0, 4));
+        System.out.println(sentence.substring(10));
+        System.out.println(sentence.substring(5,9));
+        System.out.println(sentence.substring(5,9));
+        System.out.println(sentence.contains("a"));
+        System.out.println(sentence.contains("x"));
+
+        sentence = "is "+sentence+"?";
+        System.out.println(sentence);
+        String = String.replaceFirst("t", "T");
+        System.out.println(sentence);
+
+        int favouriteNumber = 99;
+        System.out.println("My favourite number is "+favouriteNumber);
+        int num1 = 5;
+        int num2 = 3;
+
+        System.out.println(num1 + num2);
+
+        char letter = 'a';
+        int code = 98;
+
+        System.out.println((int) letter);
+        System.out.println((char) code);
+        System.out.println((char) 36);
+        System.out.println('c' - 'a');
+        System.out.println('z' - 'a');
+
+        char char1 = 'x';
+        char char2 = 'y';
+        char char3 = 'z';
+
+        String string1 = "z";
+        String string2 = "y";
+
+        System.out.println(char1+char2+char3);
+
+        String number1 = "5";
+        String number2 = "3";
+
+        int int1 = Integer.parseInt(number1);
+        int int2 = Integer.parseInt(number2);
+
+        System.out.println(int1+int2);
+
+        String number1 = "5.3";
+        String number2 = "3.2";
+
+        double double1 = Double.parseDouble(number1);
+        double double2 = Double.parseDouble(number2);
+
+        System.out.println(double1+double2);
+
+        String string1 = "abc11";
+        String string2 = "9xyz";
+        String string3 = "abc7xyz";
+
+        String string1Digits = string1.substring(3);
+        String string2Digits = string2.substring(0, 1);
+        String string3Digits = string3.substring(3, 4);
+
+        int num1 = Integer.parseInt(string1Digits);
+        int num2 = Integer.parseInt(string2Digits);
+        int num3 = Integer.parseInt(string3Digits);
+
+        System.out.println(num1+num2+num3); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
